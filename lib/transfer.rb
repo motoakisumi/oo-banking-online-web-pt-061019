5cf3b96a35895fa0b7d016b42242b7f9ad1ad505
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-12 01:17:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,13 +15,18 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+    if valid? && status == "pending"
+      if amount < self.sender.balance
+        self.sender.balance -= amount
+        self.receiver.balance += amount
+        self.status = "complete"
+      else
+         self.status = "rejected"
+         "Transaction rejected. Please check your account balance."
+      end
     else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+       self.status = "rejected"
+       "Transaction rejected. Please check your account balance."
     end
   end
 end
