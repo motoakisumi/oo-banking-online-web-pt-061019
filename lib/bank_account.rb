580b1f0ca546c29a63b2dcb6ccbb19ca822a231d
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-12 01:05:31 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance = @balance + amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.status == "closed"
+      return false
+    elsif self.balance == 0
+      return false
+    else
+      return true
+    end
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+  
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    # your code her
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    self.sender.valid?
+    self.receiver.valid?
+  end
+
+  def execute_transaction
+
+    if self.sender.balance > @amount
+
+      if self.status != "complete"
+
+        self.sender.balance = self.sender.balance - self.amount
+        self.receiver.balance = self.receiver.balance + self.amount
+        self.status = "complete"
+      end
+
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
